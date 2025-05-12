afe383098efd809807986e45ea7e15182cf9417d
pousopk <pousopk@gmail.com>
2025-05-12 17:20:47 +0200

    Adds new struct 	modified:   defs.h

--- defs.h
+++ defs.h
@@ -5,6 +5,9 @@ typedef unsigned long long U64;
 
 #define NAME "Chess engine 1.0"
 #define BRD_SQ_NUM 120
+//Should be enough to store all the half moves of a game
+
+#define MAXGAMEMOVES 2048
 
 enum {VACIO, wP,wN,wB,wQ,wK,wR,bP,bN,bB,bQ,bK,bR};
 enum {FILA_A,FILA_B,FILA_C,FILA_D,FILA_E,FILA_F,FILA_G,FILA_NONE};
@@ -24,4 +27,44 @@ A8= 91,B8,C8,D8,E8,F8,G8,H8,NO_SQ
 };
 
 enum {FALSE,TRUE};
+//To check if we can castle (1 0 0 1 implies that white can only castle king side and black can only castle queen side )
+enum {WKCA = 1, WQCA = 2 , BKCA = 4 , BQCA = 8};
+
+typedef struct 
+{
+    int move;
+    int castlePerm;
+    int enPas;
+    int fiftyMove;
+    U64 posKey;
+} S_UNDO;
+
+typedef struct {
+
+    int piezas[BRD_SQ_NUM];
+    U64 pawns[3]; // Set to 1 when there is a pawn
+//Square where the king is position for both colors
+    int kingSq[2];
+//To keep track of the side, en-passant square active and fifty move rule
+    int side;
+    int enPas;
+    int fiftyMove;
+//To keep track of the move number
+    int ply;
+    int hisPly;
+
+    int castlePerm;
+//Position KEY
+    U64 posKey;
+//Nº of pieces by type both colors and empty squares
+    int pceNum[13];
+//Nº of pieces by total diff between big, major and minor for both colors and total
+    int bigPce[3];
+    int majPce[3];
+    int minPce[3];
+//Store the move that is going to be made
+    S_UNDO history[MAXGAMEMOVES];
+
+} S_TABLERO;
+
 #endif
